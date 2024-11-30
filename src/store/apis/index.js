2688269aa842ce3b81f47bc7d8d8acc664ef505f
Harold Graphs <haroldcastillo272@gmail.com>
2024-11-30 15:13:21 +0800

    fix

--- src/store/apis/index.js
+++ src/store/apis/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
